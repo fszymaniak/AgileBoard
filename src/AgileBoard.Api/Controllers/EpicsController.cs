48aedaf4b5146d54b7561c24c6e4d4f354fcd490
fszymaniak <szymaniak.filip@gmail.com>
2023-09-24 13:40:54 +0200

    initial commit with Epic REST controller added

---
+++ src/AgileBoard.Api/Controllers/EpicsController.cs
@@ -0,0 +1,79 @@
+﻿using AgileBoard.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgileBoard.Api.Controllers;
+
+[ApiController]
+[Route("epics")]
+public sealed class EpicsController : ControllerBase
+{
+    private static int _id = 1;
+    private static readonly List<Epic> Epics = new();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Epic>> Get() => Ok(Epics);
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Epic> Get(int id)
+    {
+        var epic = Epics.SingleOrDefault(e => e.Id == id);
+        if (epic is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(epic);
+    }
+
+    [HttpPost]
+    public ActionResult Post(Epic epic)
+    {
+        var epicNameAlreadyExists = Epics.Any(e => e.Name == epic.Name);
+        
+        if (epicNameAlreadyExists)
+        {
+            return BadRequest();
+        }
+        
+        epic.Id = _id;
+        epic.Status = "New";
+        epic.CreatedDate = DateTime.UtcNow;
+        _id++;
+        
+        Epics.Add(epic);
+
+        return CreatedAtAction(nameof(Get), new { id = epic.Id }, null);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult<Epic> Put(int id, Epic epic)
+    {
+        var existingEpic = Epics.SingleOrDefault(e => e.Id == id);
+        
+        if (existingEpic is null)
+        {
+            return NotFound();
+        }
+
+        existingEpic.Name = epic.Name;
+        existingEpic.Status = epic.Status;
+        existingEpic.Description = epic.Description;
+        existingEpic.AcceptanceCriteria = epic.AcceptanceCriteria;
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var existingEpic = Epics.SingleOrDefault(e => e.Id == id);
+        
+        if (existingEpic is null)
+        {
+            return NotFound();
+        }
+
+        Epics.Remove(existingEpic);
+        return NoContent();
+    }
+}
---
+++ src/AgileBoard.Api/Models/Epic.cs
@@ -0,0 +1,16 @@
+﻿namespace AgileBoard.Api.Models;
+
+public sealed class Epic
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Status { get; set; }
+
+    public string Description { get; set; }
+
+    public string AcceptanceCriteria { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+}
