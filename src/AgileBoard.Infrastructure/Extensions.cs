e79db014d2e2b9c119a1db0d849793467b4ebfca
fszymaniak <szymaniak.filip@gmail.com>
2023-10-08 14:24:53 +0200

    add AddPostgres dependency injection to Extensions.cs in Infrastructure project

--- src/AgileBoard.Infrastructure/Extensions.cs
+++ src/AgileBoard.Infrastructure/Extensions.cs
@@ -1,7 +1,8 @@
 ﻿using System.Runtime.CompilerServices;
 using AgileBoard.Application.Services.Clock;
 using AgileBoard.Core.Repositories;
-using AgileBoard.Infrastructure.Repositories;
+using AgileBoard.Infrastructure.DAL;
+using AgileBoard.Infrastructure.DAL.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: InternalsVisibleTo("AgileBoard.Tests.Unit")]
@@ -12,7 +13,8 @@ public static class Extensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services
-            .AddSingleton<IClock, Clock>()
+            .AddPostgres()
+            .AddSingleton<IClock, Clock.Clock>()
             .AddSingleton<IEpicRepository, InMemoryEpicRepository>();
 
         return services;
