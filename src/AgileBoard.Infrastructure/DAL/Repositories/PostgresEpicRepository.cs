610650f9de187a03edda8becd857c4dca2da7e64
fszymaniak <szymaniak.filip@gmail.com>
2023-10-09 20:59:37 +0200

    Add PostgresEpicRepository.cs

---
+++ src/AgileBoard.Infrastructure/DAL/Repositories/PostgresEpicRepository.cs
@@ -0,0 +1,37 @@
+﻿using AgileBoard.Core.Entities;
+using AgileBoard.Core.Exceptions;
+using AgileBoard.Core.Repositories;
+using AgileBoard.Core.ValueObjects;
+
+namespace AgileBoard.Infrastructure.DAL.Repositories;
+
+internal sealed class PostgresEpicRepository : IEpicRepository
+{
+    private readonly AgileBoardDbContext _dbContext;
+    public PostgresEpicRepository(AgileBoardDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Epic Get(EpicId? id) => _dbContext.Epics.SingleOrDefault(x => x.Id == id) ?? throw new EpicDoesNotExist();
+
+    public IEnumerable<Epic> GetAll() => _dbContext.Epics.ToList();
+
+    public void Add(Epic epic)
+    {
+        _dbContext.Epics.Add(epic);
+        _dbContext.SaveChanges();
+    }
+
+    public void Update(Epic epic)
+    {
+        _dbContext.Update(epic);
+        _dbContext.SaveChanges();
+    }
+
+    public void Delete(Epic epic)
+    {
+        _dbContext.Epics.Remove(epic);
+        _dbContext.SaveChanges();
+    }
+}
